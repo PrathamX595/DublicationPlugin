cb83cba9314c3b285649f9c957dae8ef0b43b1d6
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2025-04-01 19:02:29 +0530

    established comms b/w ui and plugin code

--- code.ts
+++ code.ts
@@ -1,2 +1,27 @@
 figma.showUI(__html__)
-figma.ui.resize(500,500)
+figma.ui.resize(500, 500)
+
+let Nodes: any[] = [];
+
+for (const node of figma.currentPage.selection) {
+    if ('children' in node) {
+        node.children.map(child => {
+            if(child.type == "TEXT"){
+                let childNode = {
+                    id: child.id,
+                    name: child.name,
+                    type: child.type,
+                    visible: child.visible,
+                    locked: child.locked,
+                    width: 'width' in child ? child.width : null,
+                    height: 'height' in child ? child.height : null,
+                    x: 'x' in child ? child.x : null,
+                    y: 'y' in child ? child.y : null,
+                };
+                Nodes.push(childNode)
+            }
+        });
+    }
+}
+
+figma.ui.postMessage(Nodes);
